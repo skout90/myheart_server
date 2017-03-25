78958dda68c38f769c9abd039fc918dfa299cde2
Junho Nam <skout90@naver.com>
2017-03-26 00:29:59 +0900

    컨트롤러 List형식으로 리턴가능하도록, 크로스 도메인 무관하게 API접근 가능하도록 수정

---
+++ src/main/java/com/myheart/iylm/controller/ScheduleController.java
@@ -0,0 +1,48 @@
+package com.myheart.iylm.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myheart.iylm.service.ScheduleService;
+import com.myheart.iylm.vo.ScheduleVo;
+
+/**
+ * <pre>
+ * Class Name  : ScheduleController.java
+ * Description : 스케줄 컨트롤러
+ * Modification Information
+ *
+ *    수정일　　　 　　  수정자　　　     수정내용
+ *    ────────────   ─────────   ───────────────────────────────
+ *    2017. 3. 24.   남준호              최초생성
+ * </pre>
+ *
+ * @author 남준호
+ * @since 2017. 3. 24.
+ * @version 1.0
+ *
+ * Copyright (C) 2017 by MyHeart All right reserved.
+ */
+@RequestMapping(value = "/schedule")
+@RestController
+public class ScheduleController {
+
+    @Autowired
+    ScheduleService scheduleService;
+
+    /**
+     * @Author 남준호
+     * @Comment 스케줄 리스트를 리턴합니다.
+     * @param scheduleVo
+     * @return
+     */
+    @RequestMapping(value = "SelectScheduleList")
+    public List<ScheduleVo> selectScheduleList(ScheduleVo scheduleVo) throws Exception {
+        //        HashMap<String, Object> map = new HashMap<>();
+        //        map.put("resultMap", );
+        return this.scheduleService.selectList(scheduleVo);
+    }
+}
---
+++ src/main/java/com/myheart/iylm/vo/ScheduleVo.java
@@ -0,0 +1,73 @@
+package com.myheart.iylm.vo;
+
+import java.util.Date;
+
+/**
+ * <pre>
+ * Class Name  : ScheduleVo.java
+ * Description : 스케줄 Vo
+ * Modification Information
+ *
+ *    수정일　　　 　　  수정자　　　     수정내용
+ *    ────────────   ─────────   ───────────────────────────────
+ *    2017. 3. 24.   남준호              최초생성
+ * </pre>
+ *
+ * @author 남준호
+ * @since 2017. 3. 24.
+ * @version 1.0
+ *
+ * Copyright (C) 2017 by MyHeart All right reserved.
+ */
+public class ScheduleVo {
+    // 스케줄 일련번호
+    Integer schNo;
+    // 복용 시작일
+    Date startDt;
+    // 복용 종료일
+    Date endDt;
+    // 시간 구분
+    String timeDivision;
+    // 푸시 시간
+    String pushTime;
+
+    public Integer getSchNo() {
+        return schNo;
+    }
+
+    public void setSchNo(Integer schNo) {
+        this.schNo = schNo;
+    }
+
+    public Date getStartDt() {
+        return startDt;
+    }
+
+    public void setStartDt(Date startDt) {
+        this.startDt = startDt;
+    }
+
+    public Date getEndDt() {
+        return endDt;
+    }
+
+    public void setEndDt(Date endDt) {
+        this.endDt = endDt;
+    }
+
+    public String getTimeDivision() {
+        return timeDivision;
+    }
+
+    public void setTimeDivision(String timeDivision) {
+        this.timeDivision = timeDivision;
+    }
+
+    public String getPushTime() {
+        return pushTime;
+    }
+
+    public void setPushTime(String pushTime) {
+        this.pushTime = pushTime;
+    }
+}
