ccbb412ce8b0e6e42d2273d2989a927acca812ef
jshag90 <jshag90@naver.com>
2017-03-26 10:47:21 +0900

    170326-dev rebase test

--- src/main/java/com/myheart/iylm/controller/UserController.java
+++ src/main/java/com/myheart/iylm/controller/UserController.java
@@ -9,17 +9,45 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.myheart.iylm.dto.UserVo;
 import com.myheart.iylm.service.impl.UserService;
 
+/**
+ * <pre>
+ * Class Name  : UserController.java
+ * Description :
+ * Modification Information
+ *
+ *    수정일　　　 　　  수정자　　　     수정내용
+ *    ────────────   ─────────   ───────────────────────────────
+ *    2017. 3. 26.   지선학              최초생성
+ * </pre>
+ *
+ * @author 지선학
+ * @since 2017. 3. 26.
+ * @version 1.0
+ *
+ * Copyright (C) 2017 by MYHEART All right reserved.
+ */
+
+@RequestMapping(value="/user")
 @Controller
 public class UserController {
 	
 	 @Autowired
 	 UserService userService;
 	
-	@RequestMapping("/insertUser")
+
+	/**
+	 * @Author apple
+	 * @Comment
+	 * @param userVo
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("insertUser")
+
 	public String insertUser(@RequestParam UserVo userVo,Model model){
 		
 		userService.insertUserService(userVo);
-		
+
 		return "userService";
 	}
 	
