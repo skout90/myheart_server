7ddb61ab29de2f87953fa6b1b0c9b2a68002005b
Junho Nam <skout90@naver.com>
2017-05-21 12:34:48 +0900

    task scheduler 코드 작성

---
+++ src/main/java/com/myheart/iylm/schedule/PushSchedule.java
@@ -0,0 +1,32 @@
+package com.myheart.iylm.schedule;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * <pre>
+ * Class Name  : PushScheduleJob.java
+ * Description : 스케줄 잡 정의
+ * Modification Information
+ *
+ *    수정일　　　 　　  수정자　　　     수정내용
+ *    ────────────   ─────────   ───────────────────────────────
+ *    2017. 3. 30.   남준호              최초생성
+ * </pre>
+ *
+ * @author 남준호
+ * @since 2017. 3. 30.
+ * @version 1.0
+ *
+ * Copyright (C) 2017 by KHSA All right reserved.
+ */
+@Component
+public class PushSchedule {
+
+    //    @Autowired
+    //    private ScheduleService scheduleService;
+
+    public void executeJob() throws Exception {
+        //        ScheduleVo scheduleVo = new ScheduleVo();
+        //        this.scheduleService.insert(scheduleVo);
+    }
+}
